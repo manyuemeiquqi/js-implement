bf4a2fd1c36fbcabfdf4c4495addb9bbbd9a3830
jinpeng <940495614@qq.com>
2023-08-22 14:29:39 +0800

    feat: remove

--- src/polyfill/DOM/remove.ts
+++ src/polyfill/DOM/remove.ts
@@ -4,3 +4,19 @@ function remove<T extends HTMLElement>(node: T): T | undefined {
 const el = document.getElementById("div-02");
 
 remove(el as HTMLElement);
+
+// (function (arr) {
+//   arr.forEach(function (item) {
+//     if (item.hasOwnProperty("remove")) {
+//       return;
+//     }
+//     Object.defineProperty(item, "remove", {
+//       configurable: true,
+//       enumerable: true,
+//       writable: true,
+//       value: function remove() {
+//         this.parentNode.removeChild(this);
+//       },
+//     });
+//   });
+// })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
