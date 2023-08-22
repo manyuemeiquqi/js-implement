9182ffe9ba0d359ea964646958626a86dda6191b
jinpeng <940495614@qq.com>
2023-08-22 15:25:30 +0800

    feat: getType

---
+++ src/type-utils/getType.ts
@@ -0,0 +1,10 @@
+function getType(data: unknown): string {
+  // [Object ]
+  return Object.prototype.toString.call(data).slice(8, -1).toLocaleLowerCase();
+}
+
+getType(null);
+getType(1);
+getType(Number(1));
+getType(() => console.log(2));
+getType({ a: 1 });
