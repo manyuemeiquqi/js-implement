95395037e8bf5903bd9c26420979a4c03e1939ce
jinpeng <940495614@qq.com>
2023-08-24 10:33:45 +0800

    feat: chaining

---
+++ src/system-design/chaining.ts
@@ -0,0 +1,29 @@
+class Calculator {
+  res: number;
+  constructor() {
+    this.res = 0;
+  }
+  add(num: number) {
+    this.res += num;
+    return this;
+  }
+  minus(num: number) {
+    this.res -= num;
+    return this;
+  }
+
+  multiply(num: number) {
+    this.res *= num;
+    return this;
+  }
+  divide(num: number) {
+    this.res /= num;
+    return this;
+  }
+  getValue() {
+    return this.res;
+  }
+}
+const result = new Calculator().add(5).multiply(2).divide(2).getValue();
+
+console.log("Result:", result);
