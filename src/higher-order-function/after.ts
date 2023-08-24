e65ffebb7e177c64cf2a240e3bdd2d852459d972
jinpeng <940495614@qq.com>
2023-08-24 09:46:05 +0800

    fix: after

--- src/higher-order-function/after.ts
+++ src/higher-order-function/after.ts
@@ -1,15 +1,20 @@
+// n及n以后执行
 function after(n: number, fn: (...args: unknown[]) => unknown) {
   return function (this: unknown, ...args: unknown[]) {
-    if (n-- < 1) return fn.apply(this, args);
+    if (--n < 1) return fn.apply(this, args);
   };
 }
 
-const consoleLog = (str: unknown) => {
-  console.log("log: ", str);
+const afterFn = after(1, function (this: { count: number }) {
+  return ++this.count;
+});
+const obj = {
+  after: afterFn,
+  count: 0,
 };
-const after3Log = after(3, consoleLog);
-after3Log(1);
-after3Log(2);
-after3Log(3);
-after3Log(4);
-after3Log(5);
+
+console.log("obj.after(): ", obj.after());
+console.log("object.after(): ", obj.after());
+console.log("object.after(): ", obj.after());
+console.log("object.after(): ", obj.after());
+console.log(obj.count);
