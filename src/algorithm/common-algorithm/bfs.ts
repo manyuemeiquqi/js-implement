c3cc880089f1d61f8d2691ce727bddfd99f5fc7f
jinpeng <940495614@qq.com>
2023-08-26 11:45:19 +0800

    feat: dfs bfs

---
+++ src/algorithm/common-algorithm/bfs.ts
@@ -0,0 +1,79 @@
+interface TreeNode {
+  children?: TreeNode[];
+  label?: unknown;
+}
+function breadthFirstSearch(data: TreeNode) {
+  const stk = [data];
+  while (stk.length) {
+    let len = stk.length;
+    while (len--) {
+      const item = stk.shift();
+      console.log("item: ", item?.label);
+      if (item?.children) {
+        stk.push(...item.children);
+      }
+    }
+  }
+}
+const data: TreeNode = {
+  label: "root",
+  children: [
+    {
+      label: "Level one 1",
+      children: [
+        {
+          label: "Level two 1-1",
+          children: [
+            {
+              label: "Level three 1-1-1",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Level one 2",
+      children: [
+        {
+          label: "Level two 2-1",
+          children: [
+            {
+              label: "Level three 2-1-1",
+            },
+          ],
+        },
+        {
+          label: "Level two 2-2",
+          children: [
+            {
+              label: "Level three 2-2-1",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Level one 3",
+      children: [
+        {
+          label: "Level two 3-1",
+          children: [
+            {
+              label: "Level three 3-1-1",
+            },
+          ],
+        },
+        {
+          label: "Level two 3-2",
+          children: [
+            {
+              label: "Level three 3-2-1",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+breadthFirstSearch(data);
+export {};
---
+++ src/algorithm/common-algorithm/dfs.ts
@@ -0,0 +1,78 @@
+interface TreeNode {
+  children?: TreeNode[];
+  label?: unknown;
+}
+function deepFirstSearch(data: TreeNode): void {
+  if (data.children !== undefined) {
+    console.log("data: ", data.label);
+
+    if (data.children.length === 0) return;
+    else {
+      for (let i = 0; i < data.children.length; i++) {
+        deepFirstSearch(data.children[i]);
+      }
+    }
+  } else return;
+}
+const data: TreeNode = {
+  label: "root",
+  children: [
+    {
+      label: "Level one 1",
+      children: [
+        {
+          label: "Level two 1-1",
+          children: [
+            {
+              label: "Level three 1-1-1",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Level one 2",
+      children: [
+        {
+          label: "Level two 2-1",
+          children: [
+            {
+              label: "Level three 2-1-1",
+            },
+          ],
+        },
+        {
+          label: "Level two 2-2",
+          children: [
+            {
+              label: "Level three 2-2-1",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Level one 3",
+      children: [
+        {
+          label: "Level two 3-1",
+          children: [
+            {
+              label: "Level three 3-1-1",
+            },
+          ],
+        },
+        {
+          label: "Level two 3-2",
+          children: [
+            {
+              label: "Level three 3-2-1",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+deepFirstSearch(data);
