7d4ee8b5882f010657d48feb861548bf06f15467
jinpeng <940495614@qq.com>
2023-08-28 16:49:04 +0800

    refactor: getRandomString

--- src/string-utils/getRandomString.ts
+++ src/string-utils/getRandomString.ts
@@ -1,13 +1,14 @@
 function getRandomString(length: number): string {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let str = "";
-  for (let i = 0; i < length; i++) {
-    str += characters[(Math.random() * characters.length) >> 0];
-  }
-  console.log("str: ", str);
+  const res = [];
 
-  return str;
+  for (let i = 0; i < length; i++) {
+    res.push(characters[(Math.random() * characters.length) >> 0]);
+  }
+  //   += 拼接字符串涉及对象的构建以及字符串的拼接，时间效率比开新空间要高
+  console.log("str: ", res);
+  return res.join("");
 }
 
 getRandomString(50);
