b7f7023b5e63addce7bc47ce95a7ce035c4e27ef
jinpeng <940495614@qq.com>
2023-08-30 12:03:29 +0800

    feat: proxyArray

---
+++ src/system-design/proxyArray.ts
@@ -0,0 +1,15 @@
+const arr: number[] = [1, 2, 3];
+function proxyArray(arr: unknown[]) {
+  return new Proxy(arr, {
+    get(data, property: string) {
+      if (+property < 0) {
+        const tailIdx = data.length + +property;
+        return data[tailIdx];
+      } else return data[+property];
+    },
+  });
+}
+
+const proxyArr = proxyArray(arr);
+console.log(proxyArr[-1]);
+console.log(proxyArr[0]);
