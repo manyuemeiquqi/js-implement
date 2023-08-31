b272d0b6b25a772c27b8036b76bed996c0ae637d
jinpeng <940495614@qq.com>
2023-08-31 12:03:21 +0800

    feat: pipe

---
+++ src/functional/pipe.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type F = (...args: any[]) => any;
+type F1 = (args: any) => any;
+type FunArr = [F, ...F1[]];
+type getLast<T extends FunArr> = T extends [
+  F,
+  ...(F1[] | []),
+  infer B extends F1,
+]
+  ? ReturnType<B>
+  : never;
+
+function pipe<T extends FunArr>(
+  ...fnList: T
+): (...args: Parameters<T[0]>) => getLast<T> {
+  return function (this: unknown, ...args: Parameters<T[0]>) {
+    let tmp;
+    for (let i = 0; i < fnList.length; i++) {
+      if (i === 0) tmp = fnList[0].apply(this, args);
+      else tmp = fnList[i].apply(this, [tmp]);
+    }
+    return tmp as getLast<T>;
+  };
+}
+
+function x(this: any, val: number) {
+  return this.x * val;
+}
+function y(this: any, val: number) {
+  return this.y * val;
+}
+function z(this: any, val: number) {
+  return this.z * val;
+}
+const context = {
+  a: pipe(x, y, z),
+  x: 4,
+  y: 2,
+  z: 1,
+};
+context.a(5);
+console.log("context.a(5): ", context.a(5));
