957ce01e190bfa4d710dc0608baaf4156680ef67
jinpeng <940495614@qq.com>
2023-09-04 17:46:26 +0800

    feat: sum

--- src/system-design/chaining.ts
+++ src/system-design/chaining.ts
@@ -26,3 +26,19 @@ class Calculator {
 const result = new Calculator().add(5).multiply(2).divide(2).getValue();
 
 console.log("Result:", result);
+
+// 这里不要用this，只能将 innerSum当成一个对象，然后访问当前val
+function accumlator(...args: number[]) {
+  const val = args.reduce((a, b) => a + b);
+  function innerSum(...args1: number[]) {
+    innerSum.val += args1.reduce((a, b) => a + b, 0);
+    return innerSum;
+  }
+  innerSum.val = val;
+  return innerSum;
+}
+
+const result1 = accumlator(1, 2, 3, 4, 5)(2)(4)(4).val;
+console.log(result1); //
+
+export {};
