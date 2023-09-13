5be0538527e2b9081b9503ffb082b1f39e87635d
jinpeng <940495614@qq.com>
2023-09-13 17:46:39 +0800

    feat: type path

--- src/polyfill/array/mutating-method/push.ts
+++ src/polyfill/array/mutating-method/push.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../../../index.d.ts" />
+/// <reference path="../../../../index.d.ts" />
 Array.prototype.myPush = function (...val: unknown[]): number {
   for (let i = 0; i < val.length; i++) {
     this[this.length] = val[i];
