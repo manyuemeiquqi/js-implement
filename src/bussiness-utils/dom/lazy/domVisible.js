6c502a9a909a8963e9f911dcfe63c9e9afa5f9f1
manyuemeiquqi <940495614@qq.com>
2023-09-28 11:13:10 +0800

    feat: dom visible

---
+++ src/bussiness-utils/dom/lazy/domVisible.js
@@ -0,0 +1,36 @@
+function getVisible(dom) {
+  const rect = dom.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight ||
+        document.documentElement.clientHeight) /* or $(window).height() */ &&
+    rect.right <=
+      (window.innerWidth ||
+        document.documentElement.clientWidth) /* or $(window).width() */
+  );
+}
+
+// function onVisibleChange(el, cb) {
+//   var oldVisible = false;
+//   return function () {
+//     const visible = getVisible(el);
+//     if (visible != oldVisible) {
+//       oldVisible = visible;
+//       cb();
+//     }
+//   };
+// }
+// const handler = onVisibleChange(el, function () {});
+// document.addEventListener('scroll',handler)
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+    } else {
+    }
+  });
+});
+observer.observe(dom);
+// entries observe传入的dom集合，按照注册顺序排列
