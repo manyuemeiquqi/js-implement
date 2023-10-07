c7ec8858d949cee0b70fcd528fe65b2c4c62d6d5
manyuemeiquqi <940495614@qq.com>
2023-10-07 10:31:54 +0800

    feat: add content

--- src/algorithm/common-algorithm/kmp.ts
+++ src/algorithm/common-algorithm/kmp.ts
@@ -1,6 +1,7 @@
 //    kmp 算法跳过逻辑 哪里失配了，选取失配位置前一个位置 j-1 让 next[j-1] 对准失配位置
 // https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/solutions/732236/shi-xian-strstr-by-leetcode-solution-ds6y/
 // 官方题解比较晦涩难懂，需要理解
+// https://www.zhihu.com/question/21923021
 function kmp(str: string, p: string) {
   const patternLen = p.length;
   const next: number[] = new Array(patternLen).fill(0) as number[];
