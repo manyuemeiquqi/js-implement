d23f91db3c606bf1fb9a2bad50536ee5128342b5
manyuemeiquqi <940495614@qq.com>
2023-10-07 14:00:44 +0800

    feat: add generator

--- src/polyfill/array/iterable.js
+++ src/polyfill/array/iterable.js
@@ -4,21 +4,27 @@ const obj = {
     age: "a",
     value: "we",
   },
-  [Symbol.iterator]() {
+  // [Symbol.iterator]() {
+  //   const keys = Object.keys(this.data);
+  //   let index = 0;
+  //   return {
+  //     next: () => {
+  //       if (index < keys.length) {
+  //         return {
+  //           value: this.data[keys[index++]],
+  //           done: false,
+  //         };
+  //       } else {
+  //         return { done: true };
+  //       }
+  //     },
+  //   };
+  // },
+  *[Symbol.iterator]() {
     const keys = Object.keys(this.data);
-    let index = 0;
-    return {
-      next: () => {
-        if (index < keys.length) {
-          return {
-            value: this.data[keys[index++]],
-            done: false,
-          };
-        } else {
-          return { done: true };
-        }
-      },
-    };
+    for (let i = 0; i < keys.length; i++) {
+      yield this.data[keys[i]];
+    }
   },
 };
 for (const iterator of obj) {
@@ -26,29 +32,29 @@ for (const iterator of obj) {
 }
 console.log([1, 2, 3][Symbol.iterator]());
 
-function getIterator(array) {
-  let idx = 0;
-  const len = array.length;
-  return {
-    next: () => {
-      if (idx < len) {
-        return {
-          value: array[idx++],
-          done: false,
-        };
-      }
-      return {
-        value: undefined,
-        done: true,
-      };
-    },
-  };
-}
-const a = getIterator([1, 2, 3, 4, 5]);
-console.log("a: ", a);
-console.log("a: ", a.next());
-console.log("a: ", a.next());
-console.log("a: ", a.next());
-console.log("a: ", a.next());
-console.log("a: ", a.next());
-console.log("a: ", a.next());
+// function getIterator(array) {
+//   let idx = 0;
+//   const len = array.length;
+//   return {
+//     next: () => {
+//       if (idx < len) {
+//         return {
+//           value: array[idx++],
+//           done: false,
+//         };
+//       }
+//       return {
+//         value: undefined,
+//         done: true,
+//       };
+//     },
+//   };
+// }
+// const a = getIterator([1, 2, 3, 4, 5]);
+// console.log("a: ", a);
+// console.log("a: ", a.next());
+// console.log("a: ", a.next());
+// console.log("a: ", a.next());
+// console.log("a: ", a.next());
+// console.log("a: ", a.next());
+// console.log("a: ", a.next());
