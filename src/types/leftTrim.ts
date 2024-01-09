e63527b62c5c9a0ae07e60a56030f04a7759a628
jinpeng <940495614@qq.com>
2024-01-09 09:06:49 +0800

    feat: add question desc

--- src/types/leftTrim.ts
+++ src/types/leftTrim.ts
@@ -1,3 +1,5 @@
+// q: https://github.com/type-challenges/type-challenges/blob/main/questions/00106-medium-trimleft/README.md
+
 type Space = " " | "\n";
 type LeftType<T> = T extends `${Space}${infer B}` ? LeftType<B> : T;
 
